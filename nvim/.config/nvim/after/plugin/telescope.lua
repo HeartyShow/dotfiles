7a5a3f7088bc01475d302711643c886125c1cfbf
Alexandre Vialar <alexandre.vialar@sap.com>
2024-07-24 22:26:21 +0200

    nvim telescope buffer tool

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,45 @@
 local builtin = require('telescope.builtin')
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
+-- Helper functions
+
+local buffer_searcher
+buffer_searcher = function()
+    builtin.buffers {
+        sort_mru = true,
+        ignore_current_buffer = true,
+        show_all_buffers = false,
+        attach_mappings = function(prompt_bufnr, map)
+        local refresh_buffer_searcher = function()
+            actions.close(prompt_bufnr)
+            vim.schedule(buffer_searcher)
+        end
+        local delete_buf = function()
+            local selection = action_state.get_selected_entry()
+            vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+            refresh_buffer_searcher()
+        end
+        local delete_multiple_buf = function()
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local selection = picker:get_multi_selection()
+            for _, entry in ipairs(selection) do
+                vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+        end
+        refresh_buffer_searcher()
+    end
+    map('n', 'dd', delete_buf)
+    map('n', '<C-d>', delete_multiple_buf)
+    map('i', '<C-d>', delete_multiple_buf)
+    return true
+end
+}
+end
 
 -- Base keymaps
 
 vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Find file in current project' })
+vim.keymap.set('n', '<leader>b', buffer_searcher, { desc = 'Find opened buffers' })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = 'Find recently opened files', silent = true })
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Find in current file', silent = true })
 vim.keymap.set('n', '<leader>he', builtin.help_tags, { desc = 'Search help tags', silent = true })
