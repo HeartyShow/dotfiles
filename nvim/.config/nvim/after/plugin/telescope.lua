d88dc2eb8126ad2bf8395fcb06dd0f1646aa1407
Alexandre Vialar <alexandre.vialar@sap.com>
2024-07-25 17:24:11 +0200

    mvim telescope fixes

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -8,32 +8,32 @@ local buffer_searcher
 buffer_searcher = function()
     builtin.buffers {
         sort_mru = true,
-        ignore_current_buffer = true,
+        ignore_current_buffer = false,
         show_all_buffers = false,
         attach_mappings = function(prompt_bufnr, map)
-        local refresh_buffer_searcher = function()
-            actions.close(prompt_bufnr)
-            vim.schedule(buffer_searcher)
+            local refresh_buffer_searcher = function()
+                actions.close(prompt_bufnr)
+                vim.schedule(buffer_searcher)
+            end
+            local delete_buf = function()
+                local selection = action_state.get_selected_entry()
+                vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+                refresh_buffer_searcher()
+            end
+            local delete_multiple_buf = function()
+                local picker = action_state.get_current_picker(prompt_bufnr)
+                local selection = picker:get_multi_selection()
+                for _, entry in ipairs(selection) do
+                    vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+                end
+                refresh_buffer_searcher()
+            end
+            map('n', 'dd', delete_buf)
+            map('n', '<C-d>', delete_multiple_buf)
+            map('i', '<C-d>', delete_multiple_buf)
+            return true
         end
-        local delete_buf = function()
-            local selection = action_state.get_selected_entry()
-            vim.api.nvim_buf_delete(selection.bufnr, { force = true })
-            refresh_buffer_searcher()
-        end
-        local delete_multiple_buf = function()
-            local picker = action_state.get_current_picker(prompt_bufnr)
-            local selection = picker:get_multi_selection()
-            for _, entry in ipairs(selection) do
-                vim.api.nvim_buf_delete(entry.bufnr, { force = true })
-        end
-        refresh_buffer_searcher()
-    end
-    map('n', 'dd', delete_buf)
-    map('n', '<C-d>', delete_multiple_buf)
-    map('i', '<C-d>', delete_multiple_buf)
-    return true
-end
-}
+    }
 end
 
 -- Base keymaps
@@ -89,6 +89,20 @@ require("telescope").setup({
         file_ignore_patterns = {
             'node_modules',
             '.git'
+        },
+    },
+    pickers = {
+        find_files = {
+            find_command = {
+                'fd',
+                '--type',
+                'f',
+                '--color=never',
+                '--hidden',
+                '--follow',
+                '-E',
+                '.git/*'
+            },
         },
     },
     extensions = {
