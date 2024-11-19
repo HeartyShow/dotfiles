b1525cd05b2cba4f2659cb9133b6071a5800bc73
Alexandre Vialar <alexandre.vialar@sap.com>
2024-11-19 14:03:24 +0100

    removing format keymap

--- nvim/.config/nvim/lua/avialar/remap.lua
+++ nvim/.config/nvim/lua/avialar/remap.lua
@@ -22,7 +22,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
+--vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
