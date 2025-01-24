26048733b07f6d9305eb779960bf9b3eaa4e7515
Alexandre Vialar <alexandre.vialar@sap.com>
2025-01-24 15:47:53 +0100

    update todo search command

--- nvim/.config/nvim/lua/plugins/todo-comment.lua
+++ nvim/.config/nvim/lua/plugins/todo-comment.lua
@@ -1,10 +1,29 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-  },
-  config = function (_, opts)
-    require('todo-comments').setup(opts)
-    vim.keymap.set('n', '<leader>ft', ':TodoTelescope<CR>', { desc = 'Find Todos in current project', silent = true })
-  end
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		signs = false,
+		search = {
+			command = "rg",
+			args = {
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--hidden",
+				"-u",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("todo-comments").setup(opts)
+		vim.keymap.set(
+			"n",
+			"<leader>ft",
+			":TodoTelescope<CR>",
+			{ desc = "Find Todos in current project", silent = true }
+		)
+	end,
 }
