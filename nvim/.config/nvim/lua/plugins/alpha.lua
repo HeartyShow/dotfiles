ea2d9089918be6ec077aef7ef6b6a6830c7ba7d3
Alexandre Vialar <alexandre.vialar@sap.com>
2025-07-25 21:33:04 +0200

    new sesh/tmux/tx setup

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,36 +1,40 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-        'nvim-telescope/telescope.nvim',
-    },
-    opts = function()
-        local dashboard = require("alpha.themes.dashboard")
-        -- Set header
-        dashboard.section.header.val = {
-            "                                                     ",
-            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-            "                                                     ",
-        }
+	"goolord/alpha-nvim",
+	enabled = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-telescope/telescope.nvim",
+	},
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-        -- Set menu
-        dashboard.section.buttons.val = {
-            dashboard.button("r", "󱦟  Recent", ":Telescope oldfiles<CR>"),
-            dashboard.button("p", "󰝰  Projects",
-                ":lua require'telescope'.extensions.project.project{display_type = 'full'}<CR>"),
-            dashboard.button("e", "󰝒  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("l", "󰏗  Lazy", ":Lazy<CR>"),
-            dashboard.button("s", "  Settings", ":e $NVIM_DIR<CR>"),
-            dashboard.button("q", "󰗼  Quit NVIM", ":qa<CR>"),
-        }
-        return dashboard.opts
-    end,
-    config = function(_, opts)
-        require("alpha").setup(opts)
-    end
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("r", "󱦟  Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button(
+				"p",
+				"󰝰  Projects",
+				":lua require'telescope'.extensions.project.project{display_type = 'full'}<CR>"
+			),
+			dashboard.button("e", "󰝒  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("l", "󰏗  Lazy", ":Lazy<CR>"),
+			dashboard.button("s", "  Settings", ":e $NVIM_DIR<CR>"),
+			dashboard.button("q", "󰗼  Quit NVIM", ":qa<CR>"),
+		}
+		return dashboard.opts
+	end,
+	config = function(_, opts)
+		require("alpha").setup(opts)
+	end,
 }
