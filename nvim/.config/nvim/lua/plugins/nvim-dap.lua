eae1dcb1d8fde4543d6ff5c0d34209cd34ccc23e
Alexandre Vialar <alexandre.vialar@sap.com>
2025-10-03 13:30:20 +0200

    fix dap nvim

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -1,13 +1,7 @@
 -- Nvim-DAP is a Debug Adapter Protocol client implementation for Neovim.
 return {
 	"mfussenegger/nvim-dap",
-	dependencies = { "mfussenegger/nvim-dap-python" },
 	config = function()
 		require("dap.ext.vscode").load_launchjs(nil, {})
-
-		-- Python
-		require("dap-python").setup("$HOME/.pyenv/shims/python")
-		require("dap-python").test_runner = "pytest"
-		vim.keymap.set("n", "<leader>dt", ':lua require"dap-python".test_method()<CR>')
 	end,
 }
