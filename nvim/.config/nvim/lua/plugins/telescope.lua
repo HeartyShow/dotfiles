1482f57fb600076e96128ea9e1c41d43785b650f
Alexandre Vialar <alexandre.vialar@sap.com>
2025-11-20 16:38:03 +0100

    nvim telescope custom path highlights

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,9 +2,24 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = function(_, opts)
     opts.defaults = {
-      path_display = {
-        smart = true,
-      },
+      path_display = function(_, path)
+        local tail = require("telescope.utils").path_tail(path)
+        local parent = path:sub(1, #path - #tail)
+        local smart_parent = require("telescope.utils").path_smart(parent)
+        local display = smart_parent .. tail
+
+        local highlights = {
+          {
+            {
+              0, -- highlight start position
+              #smart_parent, -- highlight end position
+            },
+            "Comment", -- highlight group name
+          },
+        }
+
+        return display, highlights
+      end,
       sorting_strategy = "ascending",
       prompt_prefix = " ",
       selection_caret = " ",
