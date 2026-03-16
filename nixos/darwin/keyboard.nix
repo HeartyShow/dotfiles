b5147af2e6dddea627f3ac4ca8601c0c645a2012
Alexandre Vialar <alexandre.vialar@sap.com>
2026-03-16 17:31:58 +0100

    remove conflicting macos default shortcuts

--- nixos/darwin/keyboard.nix
+++ nixos/darwin/keyboard.nix
@@ -4,5 +4,31 @@
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
   };
+
+  system.defaults = {
+    CustomUserPreferences = {
+      "com.apple.symbolichotkeys" = {
+        AppleSymbolicHotKeys = {
+          "60" = {
+            # Disable '^ + Space' for selecting the previous input source
+            enabled = false;
+          };
+          "61" = {
+            # Disable '^ + Option + Space' for selecting the next input source
+            enabled = false;
+          };
+          # Disable 'Cmd + Space' for Spotlight Search
+          "64" = {
+            enabled = false;
+          };
+          # Disable 'Cmd + Alt + Space' for Finder search window
+          "65" = {
+            # Set to false to disable
+            enabled = true;
+          };
+        };
+      };
+    };
+  };
 }
 
